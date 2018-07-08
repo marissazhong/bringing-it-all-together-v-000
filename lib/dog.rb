8689aa28e64a218271c4e6838088d70224ee8a4c
Marissa Zhong <mwz2101@columbia.edu>
2018-07-08 15:16:46 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+class Dog
+  attr_accessor :id, :name, :breed
+  def initialize(attributes)
+    attributes.each {|k,v| self.send(("#{k}="),v)}
+  end
+
+  def self.create_table
+    drop_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
+
+  def self.new_from_db(row)
+    attributes = {id: row[0], name: row[1], breed: row[2]}
+    dog = Dog.new(attributes)
+  end
+
+end
